53089e651653649817bfe3f48cbff8aa37cdd835
divyaGrvgithub <divyamalatiwarimtech@gmail.com>
2023-01-02 12:45:39 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
-const express= require("express")
-const bodyParser=require("body-parser")
-const app =express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
 
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.set('strictQuery', true)
+mongoose.connect("mongodb+srv://karthikramadugu:Karthiksai1@karthikcluster.b2ikjot.mongodb.net/test", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use('/',route)
+app.use (
+    function (req, res, next) {
+        console.log ("inside GLOBAL MW");
+        next();
+  }
+  );
+
+app.use('/', route);
 
 
-app.listen(3000 , function(){
-    console.log('Express app running on port ' +(3000))
-}
-)
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
